0de9ce4e60866e915603ee5a66bb48824e6c2c15
Оnline and offline school in Kaniv <140415676+legorobocoding-Kaniv@users.noreply.github.com>
2025-09-27 14:57:32 +0300

    fourth commit
    js

---
+++ new.js
@@ -0,0 +1,17 @@
+let userName = prompt("Please enter your name:", "Guest");
+
+alert("Hello world!");
+
+console.log(10);
+
+function clickMe(){
+	document.getElementById("one").innerHTML = "<h3>what are you doing ? </h3>"
+}
+
+let isBoss = confirm("Ви бос?");
+
+alert( isBoss ); 
+
+let age = prompt('Скільки вам років?', 100);
+
+alert(`Вам ${age} років!`); 
